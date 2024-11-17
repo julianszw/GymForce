068c4c8f09a0f5ad68169918dfe3a7727e847a43
mateo <mateoe.loyola@gmail.com>
2024-11-17 04:37:28 -0300

    dominio prueba headquarter

---
+++ lib/domain/headquarter_domain.dart
@@ -0,0 +1,20 @@
+class Headquarter {
+  String title;
+  String address;
+  bool isOpen;
+  String hours;
+  String imageUrl;
+  String status;
+  int maxCapacity;
+
+  Headquarter({
+    required this.title,
+    required this.address,
+    required this.isOpen,
+    required this.hours,
+    required this.imageUrl,
+    required this.status,
+    required this.maxCapacity
+  });
+}
+
