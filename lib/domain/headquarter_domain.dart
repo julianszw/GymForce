88ee34adf665fa3d36be21a99e0b2a746646aa21
mateo <mateoe.loyola@gmail.com>
2024-11-19 03:21:54 -0300

    Domain Nuevo de Headquarter

--- lib/domain/headquarter_domain.dart
+++ lib/domain/headquarter_domain.dart
@@ -1,20 +1,29 @@
-class Headquarter {
-  String title;
-  String address;
-  bool isOpen;
-  String hours;
-  String imageUrl;
-  String status;
-  int maxCapacity;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  Headquarter({
-    required this.title,
+class HeadquarterData {
+  String neighborhood;
+  String address;
+  String hoursDisplayed;
+  int maxCapacity;
+  String outsidePic;
+  DateTime? openTime;
+  DateTime? closingTime;
+  GeoPoint? geoPoint;
+  bool remodeling;
+  List<String>? sectors;
+  String cellphone;
+
+  HeadquarterData({
+    required this.neighborhood,
     required this.address,
-    required this.isOpen,
-    required this.hours,
-    required this.imageUrl,
-    required this.status,
-    required this.maxCapacity
+    required this.hoursDisplayed,
+    required this.maxCapacity,
+    required this.outsidePic,
+    required this.openTime,
+    required this.closingTime,
+    required this.geoPoint,
+    required this.remodeling,
+    required this.sectors,
+    required this.cellphone,
   });
 }
-
